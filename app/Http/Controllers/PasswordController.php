9ebcaaa2345c70abfda992fb8b65d618f82ea8e0
thiiagoms <thiagom.devsec@gmail.com>
2022-12-12 08:23:44 -0300

    feat: Add new password validations :sparkles:

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers;
 use App\Services\PasswordService;
 use Illuminate\Http\Request;
 
+/**
+ * Password Controller
+ *
+ * @package App\Http\Services
+ * @author  Thiago Silva <thiagom.devsec@gmail.com>
+ * @version 1.0
+ */
 final class PasswordController extends Controller
 {
+    /**
+     * Init controller with service
+     *
+     * @param PasswordService $passwordService
+     */
     public function __construct(private PasswordService $passwordService)
     {
     }
@@ -19,7 +31,15 @@ final class PasswordController extends Controller
      */
     public function validatePassword(Request $request)
     {
-        $password = $request->password;
+        if (!$request->has('password') or empty($request->password)) {
+            return response()->json(['error' => "You must enter a valid password"], 406);
+        }
+
+        if (!$request->has('rules') or empty($request->rules)) {
+            return response()->json(['error' => 'You must enter with valid rules'], 406);
+        }
+
+        $password = strip_tags($request->password);
         $rules = $request->rules;
 
         $result = $this->passwordService->validatePassword($password, $rules);
--- app/Services/PasswordService.php
+++ app/Services/PasswordService.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+/**
+ * Password validation
+ *
+ * @package App\Services
+ * @author  Thiago Silva <thiagom.devsec@gmail.com>
+ * @version 1.0
+ */
 final class PasswordService
 {
     private function validateMinSize(string $password, int $minLength): bool
@@ -12,16 +19,53 @@ final class PasswordService
         return $strlen >= $minLength ? true : false;
     }
 
-    private function minUpperCase(string $password, int $minUpperCaseLetters)
+    /**
+     * Validate min upper case characters in string
+     *
+     * @param string $password
+     * @param integer $minUpperCaseLetters
+     * @return void
+     */
+    private function validateMinUpperCase(string $password, int $minUpperCaseLetters)
     {
-        // TODO
+        $upperLettersCount = 0;
+
+        array_map(function (string $letter) use (&$upperLettersCount): void {
+            if (ctype_upper($letter)) {
+                $upperLettersCount++;
+            }
+        }, str_split($password));
+
+        return $upperLettersCount >= $minUpperCaseLetters ? true : false;
     }
 
-    private function minLowerCase(string $password, int $minLowerCaseLetters)
+    /**
+     * Validate lower case
+     *
+     * @param string $password
+     * @param integer $minLowerCaseLetters
+     * @return void
+     */
+    private function validateMinLowerCase(string $password, int $minLowerCaseLetters)
     {
-        // TODO
+        $lowerLettersCount = 0;
+
+        array_map(function (string $letter) use (&$lowerLettersCount): void {
+            if (ctype_lower($letter)) {
+                $lowerLettersCount++;
+            }
+        }, str_split($password));
+
+        return $lowerLettersCount >= $minLowerCaseLetters ? true : false;
     }
 
+    /**
+     * Valite special chars in password
+     *
+     * @param string $password
+     * @param integer $minSpecialChars
+     * @return boolean
+     */
     private function validatMinSpecialChars(string $password, int $minSpecialChars): bool
     {
         $specialChars = preg_match_all('![^A-z0-9]!i', $password);
@@ -73,36 +117,51 @@ final class PasswordService
         return $count >= $minDigit ? true : false;
     }
 
-    final public function validatePassword(string $password, array $passwordRules): array
+    /**
+     * Make password validation
+     *
+     * @param string $password
+     * @param array $passwordRules
+     * @return array
+     */
+    public function validatePassword(string $password, array $passwordRules): array
     {
         $errors = [];
 
-        $passwordValidate = false;
-
-        foreach ($passwordRules as $key => $rules) {
-            if ($rules['rule'] === 'minSize') {
-                $minSize = $this->validateMinSize($password, $rules['value']);
+        foreach ($passwordRules as $rule) {
+            if ($rule['rule'] === 'minSize') {
+                $minSize = $this->validateMinSize($password, $rule['value']);
                 $minSize === false ? array_push($errors, 'minSize') : null;
             }
 
-            if ($rules['rule'] === 'minSpecialChars') {
-                $specialChars = $this->validatMinSpecialChars($password, $rules['value']);
+            if ($rule['rule'] === 'minUppercase') {
+                $minUpperCase = $this->validateMinUpperCase($password, $rule['value']);
+                $minUpperCase === false ? array_push($errors, 'minUppercase') : null;
+            }
+
+            if ($rule['rule'] === 'minLowercase') {
+                $minLowerCase = $this->validateMinLowerCase($password, $rule['value']);
+                $minLowerCase === false ? array_push($errors, 'minLowerCase') : null;
+            }
+
+            if ($rule['rule'] === 'minSpecialChars') {
+                $specialChars = $this->validatMinSpecialChars($password, $rule['value']);
                 $specialChars === false ? array_push($errors, 'minSpecialChars') : null;
             }
 
-            if ($rules['rule'] === 'noRepeted') {
+            if ($rule['rule'] === 'noRepeted') {
                 $repeted = $this->notRepeted($password);
                 $repeted === false ? array_push($errors, 'notRepeted') : null;
             }
 
-            if ($rules['rule'] === 'minDigit') {
-                $minDigits = $this->minDigit($password, $rules['value']);
+            if ($rule['rule'] === 'minDigit') {
+                $minDigits = $this->minDigit($password, $rule['value']);
                 $minDigits === false ? array_push($errors, 'minDigit') : null;
             }
         }
 
         return [
-            'verify' => $passwordValidate != false ? true : false,
+            'verify' => empty($errors) ? true : false,
             'noMatch' => $errors
         ];
     }
